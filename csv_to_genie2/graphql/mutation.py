79cf79b400753cb021b207b70fb75f4d0eb2fd2f
declercq sebastien <sdq@afnor.fr>
2020-04-30 17:18:10 +0200

    Update and Delete Mutations on StandardType

--- csv_to_genie2/graphql/mutation.py
+++ csv_to_genie2/graphql/mutation.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from datetime import date
+from typing import Optional
 from graphene_django import DjangoObjectType
 import graphene
 from csv_to_genie2.graphql.types import FileType, StandardType
@@ -26,9 +27,45 @@ class CreateStandard(graphene.Mutation):
         std, _ = Standard.objects.get_or_create(
             numdos=numdos, refdoc=refdoc, ancart=ancart, datoff=datoff
         )
-        print(std)
         return CreateStandard(standard=std)
+
+
+class UpdateStandard(CreateStandard):
+    def mutate(
+        self,
+        info,
+        numdos: str,
+        refdoc: str = '',
+        ancart: str = '',
+        datoff: Optional[date] = None,
+    ) -> UpdateStandard:
+        std: Standard = Standard.objects.get(numdos=numdos)
+        if refdoc:
+            std.refdoc = refdoc
+        if ancart:
+            std.ancart = ancart
+        if datoff is not None:
+            std.datoff = datoff
+        std.save()
+        return UpdateStandard(standard=std)
+
+
+class DeleteStandard(graphene.Mutation):
+    class Arguments:
+        numdos = graphene.String(required=True)
+
+    ok: graphene.Field = graphene.Boolean()
+
+    def mutate(self, info, numdos: str) -> DeleteStandard:
+        try:
+            std: StandardType = Standard.objects.get(numdos=numdos)
+            std.delete()
+        except Standard.DoesNotExist:
+            pass
+        return DeleteStandard(ok=True)
 
 
 class Mutation(graphene.ObjectType):
     create_standard: graphene.Field = CreateStandard.Field()
+    update_standard: graphene.Field = UpdateStandard.Field()
+    delete_standard: graphene.Field = DeleteStandard.Field()
